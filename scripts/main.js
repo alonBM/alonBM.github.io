fc315fc90b22d824ea3e2339e412011e57945b5b
Humberto Alonso <ict.alonso@gmail.com>
2020-06-27 13:07:54 +0200

    added navbar with flex, not finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
-var seccionesPagina = new fullpage('#fullpage',{
-	// ──────────────────────────────────────────────────
-	//   :::::: Opciones Basicas
-	// ──────────────────────────────────────────────────
-		 autoScrolling: true, // Se activa el scroll.
-		 fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
-		 fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
-		 easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
-		 scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
-		 css3: true, // Si usara CSS3 o javascript.
-		 easingcss3: 'ease-out', // Curva de velocidad del efecto.
-		 loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
-	
-	afterLoad: function(origin, destination){
-		 if(destination.anchor == 'contacto'){
-			  document.querySelector('.footer').querySelector('h2').style.opacity = 1;
-		 }
-	}
+var seccionesPagina = new fullpage("#fullpage", {
+ 
+  // Opciones Basicas
+  // ──────────────────────────────────────────────────
+  autoScrolling: true, // Se activa el scroll.
+  fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+  fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+  easing: "easeInOutCubic", // Funcion de tiempo de la animacion.
+  scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
+  css3: true, // Si usara CSS3 o javascript.
+  easingcss3: "ease-out", // Curva de velocidad del efecto.
+  loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+  
+
+  // Barra de navegación
+  // ──────────────────────────────────────────────────
+  navigation: true, // Muesta la barra de navegación.
+  menu: "#menu", // Menu de navegación.
+  anchors: ["home", "projects", "about", "contact"], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+  navigationTooltips: ["Home", "Projects", "About", "Contact"], // Tooltips que mostrara por cada boton.
+  showActiveTooltip: false, // Mostrar tooltip activa.
+  sectionsColor : ['#000', '#c2c2c2', '#000', '#c2c2c2']
 });
