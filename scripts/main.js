cfb9f78a1f5a4ad271f4355dba9d2df94136240c
Humberto Alonso <ict.alonso@gmail.com>
2020-06-29 20:58:34 +0200

    deleting unused files, improving readability by creating more variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,22 @@
 var seccionesPagina = new fullpage("#fullpage", {
  
-  // Opciones Basicas
+  // Basico
   // ──────────────────────────────────────────────────
-  autoScrolling: true, // Se activa el scroll.
+  autoScrolling: true, // Sirve par activar el scroll.
   fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
-  fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+  fitToSectionDelay: 400, // Delay antes de acomodar la seccion automaticamente.
   easing: "easeInOutCubic", // Funcion de tiempo de la animacion.
-  scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
-  css3: true, // Si usara CSS3 o javascript.
-  easingcss3: "ease-out", // Curva de velocidad del efecto.
-  loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
-  
+  scrollingSpeed: 500, // Velocidad del scroll en milisegundos
+  css3: true, // Para que use css3
+  easingcss3: "ease-out", // Curva de la velocidad
+ 
 
   // Barra de navegación
   // ──────────────────────────────────────────────────
-  navigation: true, // Muesta la barra de navegación.
+  navigation: true, // Sirve para mostrar la barra de navegación.
   menu: "#menu", // Menu de navegación.
-  anchors: ["home", "projects", "about", "contact"], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
-  navigationTooltips: ["Home", "Projects", "About", "Contact"], // Tooltips que mostrara por cada boton.
-  showActiveTooltip: false, // Mostrar tooltip activa.
+  anchors: ["home", "projects", "about", "contact"], // Anclas para acceder mediante el menu
+  navigationTooltips: ["Home", "Projects", "About", "Contact"], // Tooltips de los botones.
+  showActiveTooltip: false, //Muestra siempre el tooltip
   sectionsColor : ['#011627', '#011627', '#011627', '#011627']
 });
